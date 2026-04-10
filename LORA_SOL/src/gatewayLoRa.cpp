6830a7ea69fc9083e2e3ac69fe71b64e39eeb661
Noé Brands <121928472+Tacosknight@users.noreply.github.com>
2026-04-10 12:08:20 +0200

    Ajout de test unitaire
    test classe Message

--- LORA_SOL/src/gatewayLoRa.cpp
+++ LORA_SOL/src/gatewayLoRa.cpp
@@ -1,12 +1,45 @@
 #include <Arduino.h>
 #include "GestionLoRa.h"
+#include "Message.h"
 
-GestionLoRa monLoRa;
+// ======================================================
+// CONFIGURATION : Commentez la ligne ci-dessous pour repasser en mode normal
+#define MODE_TEST_UNITAIRE 
+// ======================================================
 
-void setup() {
-    monLoRa.begin();
-}
+#ifdef MODE_TEST_UNITAIRE
+    // --- CODE POUR LE TEST UNITAIRE DE LA CLASSE MESSAGE ---
+    Message testMsg("F4KMN-9", "APLT00", "WIDE1-1", "F4KMN    ", "Hello");
 
-void loop() {
-    monLoRa.process();
-}
+    void setup() {
+        Serial.begin(115200);
+        delay(8000);
+        Serial.println("\n--- [UNIT TEST] CLASSE MESSAGE ACTIVÉ ---");
+
+        // OP 1 : Test format standard
+        Serial.print("OP1 - Obtenu : ");
+        Serial.println(testMsg.getPduMes(false));
+
+        // OP 2 : Test Troncature et ACK
+        testMsg.setRecipient("TROP_LONG_123"); 
+        testMsg.setComment("Ceci est un nouveau commentaire");
+        Serial.print("OP2 - Obtenu : ");
+        Serial.println(testMsg.getPduMes(true)); 
+    }
+
+    void loop() {
+        // En mode test, on ne fait rien en boucle
+    }
+
+#else
+    // --- CODE FONCTIONNEL D'ORIGINE ---
+    GestionLoRa monLoRa;
+
+    void setup() {
+        monLoRa.begin();
+    }
+
+    void loop() {
+        monLoRa.process();
+    }
+#endif

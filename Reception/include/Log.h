aa8064e76ca7ad3d42ff2430e87e1fe022b07521
Iako PAIN <95188886+IakoPain@users.noreply.github.com>
2026-06-08 13:48:04 +0200

    je vais le mettre dans "archives"a la place

--- Reception/include/Log.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-#include <Arduino.h>
-
-class Log {
-public:
-    void enregistrerErreur(String message);
-};
-#endif
--- Reception/src/Modele.cpp
+++
@@ -1,9 +0,0 @@
-// --- src/Modele.cpp ---
-#include "Modele.h"
-
-void Modele::enregistrerTelemetrie(String donnee) {
-    if (donnee.length() == 0) return;
-    
-    Serial.print("[BDD] Enregistrement de la trame : ");
-    Serial.println(donnee);
-}
